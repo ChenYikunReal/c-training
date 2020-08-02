9d5fe16f341fd843ee231299cb71f5cb14ed4298
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2020-08-02 14:50:11 +0800

    补昨天的

---
+++ projects/program-100/demo001/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * 有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
+ * 排列组合-暴力求解
+ * @return
+ */
+int main() {
+    int i,j,k;
+    for(i=1; i<5; i++)
+        for(j=1; j<5; j++)
+            for (k=1; k<5; k++)
+                if (i!=k && i!=j && j!=k)
+                    printf("%d, %d, %d\n", i, j, k);
+    return 0;
+}
